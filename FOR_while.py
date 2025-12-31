f9aae975cd1cc8c6c94ce7f1875f56f198ff1c5d
BikrantBasnet <bikrantbasnet25@gmail.com>
2025-12-31 20:41:20 +0545

    Add files via upload

---
+++ FOR_while.py
@@ -0,0 +1,99 @@
+# data = "ram"
+# for i in range(10) :
+#     print(data)
+
+# a = [1,2,3,4,5]
+# # count= 0
+# for i in range(len(a)) :
+#     print(i,a[i])
+
+# user_input = [1,2,3,4,5]
+# if user_input[0] %2 == 0 :
+#     print("even")
+# else :
+#     print("odd")    
+# if user_input[1] %2 == 0 :
+#     print("even")
+# else :
+#     print("odd")    
+# if user_input[2] %2 == 0 :
+#     print("even")
+# else :
+#     print("odd")    
+# if user_input[3]%2  == 0 :
+#     print("even")
+# else :
+#     print("odd")    
+# if user_input[4] %2 == 0 :
+#     print("even")
+# else :
+#     print("odd")      
+# odd = []
+# even = []
+# for i in range(len(user_input)) :
+#     if user_input[i] %2 == 0 :
+#         print(f'{user_input[i]} is even')
+#         even.append(user_input[i])
+#     else :
+#         print(f'{user_input[i]} is odd')   
+#         odd.append(user_input[i] )
+        
+# print(even)
+# print(odd)    
+
+# items = [1,2,'a','b',3,4,5]
+# string_aa = []
+# integer = []
+# for i in items :
+#     if isinstance(i,str) :
+#        string_aa.append(i)
+#     else :
+#        integer.append(i)
+# print(string_aa)
+# print(integer)       
+
+# type(i) == int
+
+
+# items = [1,2,'a','b',3,4,5,20,-22,-4]
+# string_aa = []
+# total = 0
+# integer = []
+# for i in items :
+#     if isinstance(i,str) :
+#        string_aa.append(i)
+#     else :
+#        if i%2 == 0 :
+#            if i>0 :
+#                total = total+i
+# print(total)      
+
+string_aa = "program"
+set1 = []
+set2 = []
+for i in string_aa :
+    if i=='a' or i =='e' or i=='o' or i=='i' or i=='u' :
+        set1.append(i)
+    else :
+        set2.append(i)
+print(set(set1))
+print(set(set2))        
+            
+# for i in string_aa :
+# if i in 'aeiou' :
+#     set.add(i)
+
+
+      
+        
+
+               
+   
+    
+    
+
+
+
+
+
+    
